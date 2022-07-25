1eb94fb3c7047582e0eb1360aed825c9450b0d21
Milan Granillo <milanstarrg@gmail.com>
2022-07-25 11:35:36 -0400

    Create gridRoot function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,48 @@
 "use strict";
 
+function createGridDiv(gridNum) {
 const gridDiv = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridNum; i++) {
   let rowDiv = document.createElement("div");
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < gridNum; i++) {
     let columnDiv = document.createElement("div");
     columnDiv.addEventListener("mouseover", () => {
-        columnDiv.style.backgroundColor = "black";
+      columnDiv.style.backgroundColor = "black";
     });
     rowDiv.appendChild(columnDiv);
   }
   gridDiv.appendChild(rowDiv);
 }
+}
+
+createGridDiv(16);
+
+
+const btnDiv = document.querySelector(".button");
+const newGridBtn = document.createElement("button");
+newGridBtn.textContent = "new grid";
+newGridBtn.style.marginLeft = "10px";
+btnDiv.appendChild(newGridBtn);
+
+function gridExpand() {
+
+  let gridNum = prompt("How many squares per side on the new grid? (max 100)");
+ 
+  gridDiv.remove();
+
+  for (let i = 16; i < gridNum; i++) {
+    let rowDiv = document.createElement("div");
+
+    for (let i = 16; i < gridNum; i++) {
+      let columnDiv = document.createElement("div");
+      columnDiv.addEventListener("mouseover", () => {
+        columnDiv.style.backgroundColor = "black";
+      });
+      rowDiv.appendChild(columnDiv);
+    }
+    gridDiv.appendChild(rowDiv);
+    }
+  }
+
+newGridBtn.addEventListener("click", gridExpand);
