ee9444d3c0f33feb2c86cd1fc4c7df227059773d
Milan Granillo <milanstarrg@gmail.com>
2022-08-09 15:58:43 -0400

    Refactor createGrid function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,55 +1,42 @@
 "use strict";
 
-function createGridRoot() {
-  const gridDiv = document.createElement("div");
-  gridDiv.setAttribute("id", "grid-root");
-  gridDiv.classList.add("container", "flex");
-  document.body.appendChild(gridDiv);
-  return gridDiv;
-}
-
 function createGrid(gridNum) {
-  const gridRoot = createGridRoot();
-
-  for (let i = 0; i < gridNum; i++) {
-    let rowDiv = document.createElement("div");
-    for (let i = 0; i < gridNum; i++) {
-      let columnDiv = document.createElement("div");
-      columnDiv.addEventListener("mouseover", () => {
-        columnDiv.style.backgroundColor = "black";
+  for (let i = 0; i < gridNum**2; i++) {
+    let cells = document.createElement("div");
+      cells.addEventListener("mouseover", () => {
+        cells.style.backgroundColor = "black";
       });
-      rowDiv.appendChild(columnDiv);
-    }
-    gridRoot.appendChild(rowDiv);
-  }
+      const container = document.querySelector("#container");
+      container.appendChild(cells);
+}
 }
 
 createGrid(16);
 
-const btnDiv = document.querySelector(".button");
-const newGridBtn = document.createElement("button");
-newGridBtn.textContent = "new grid";
-newGridBtn.style.marginLeft = "10px";
-btnDiv.appendChild(newGridBtn);
+// const btnDiv = document.querySelector(".button");
+// const newGridBtn = document.createElement("button");
+// newGridBtn.textContent = "new grid";
+// newGridBtn.style.marginLeft = "10px";
+// btnDiv.appendChild(newGridBtn);
 
-function removeGridChildren() {
-  let element = document.getElementById("grid-root");
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-}
+// function removeGridChildren() {
+//   let element = document.getElementById("grid-root");
+//   while (element.firstChild) {
+//     element.removeChild(element.firstChild);
+//   }
+// }
 
-function gridExpand() {
-  let gridNumInput = prompt(
-    "How many squares per side on the new grid? (max 100)"
-  );
+// function gridExpand() {
+//   let gridNumInput = prompt(
+//     "How many squares per side on the new grid? (max 100)"
+//   );
 
-  if (gridNumInput > 100) {
-    alert("this number is too high! enter another number lower than 100.");
-    return;
-  }
-    removeGridChildren();
-    createGrid(gridNumInput);
-}
+//   if (gridNumInput > 100) {
+//     alert("this number is too high! enter another number lower than 100.");
+//     return;
+//   }
+//     removeGridChildren();
+//     createGrid(gridNumInput);
+// }
 
-newGridBtn.addEventListener("click", gridExpand);
+// newGridBtn.addEventListener("click", gridExpand);
