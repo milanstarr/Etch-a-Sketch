073e984f5f7e1a4ea614d7b6f108f2a4304f844c
Milan Granillo <milanstarrg@gmail.com>
2022-08-31 15:39:57 -0400

    Complete final grid version with various pen features

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,16 @@
 "use strict";
 
+const RAINBOW_MODE = 'rainbow';
+const PEN_MODE = 'default';
+const ERASER_MODE = 'eraser';
+
+let ACTIVE_MODE = PEN_MODE;
+
+function getRandomColorInt () {
+  return Math.floor(Math.random() * 256);
+}
+
 function createGrid(gridNum) {
-  console.log(gridNum ** 2);
   const container = document.querySelector("#container");
   container.style['grid-template-columns'] = `repeat(${gridNum}, 1fr)`;
 
@@ -9,12 +18,16 @@ function createGrid(gridNum) {
     let cells = document.createElement("div");
 
     cells.addEventListener("mouseover", () => {
-      cells.style.backgroundColor = "black";
+      if (ACTIVE_MODE == RAINBOW_MODE) {
+        cells.style.backgroundColor = `rgb(${getRandomColorInt()}, ${getRandomColorInt()}, ${getRandomColorInt()})`;
+      } else if (ACTIVE_MODE == PEN_MODE) {
+        cells.style.backgroundColor = 'black';
+      } else { 
+        cells.style.backgroundColor = 'white';
+      }
     });
-
     container.appendChild(cells);
   }
-
 }
 
 createGrid(16);
@@ -24,6 +37,7 @@ const newGridBtn = document.createElement("button");
 newGridBtn.textContent = "new grid";
 newGridBtn.style.marginLeft = "10px";
 btnDiv.appendChild(newGridBtn);
+
 
 function removeCells() {
   let container = document.getElementById("container");
@@ -46,3 +60,26 @@ function gridExpand() {
 }
 
 newGridBtn.addEventListener("click", gridExpand);
+
+const rainbowBtn = document.createElement("button");
+rainbowBtn.textContent = 'rainbow';
+rainbowBtn.style.marginLeft = "10px";
+rainbowBtn.style.marginTop = "10px";
+btnDiv.appendChild(rainbowBtn);
+
+const eraserBtn = document.createElement("button");
+eraserBtn.textContent = 'erase';
+eraserBtn.style.marginLeft = '10px';
+eraserBtn.style.marginTop = '10px';
+btnDiv.appendChild(eraserBtn);
+
+const defaultBtn = document.createElement("button");
+defaultBtn.textContent = 'default pen';
+defaultBtn.style.marginLeft = '10px';
+defaultBtn.style.marginTop = '10px';
+btnDiv.appendChild(defaultBtn);
+
+rainbowBtn.addEventListener('click', () => ACTIVE_MODE = RAINBOW_MODE);
+eraserBtn.addEventListener('click', () => ACTIVE_MODE = ERASER_MODE);
+defaultBtn.addEventListener('click', () => ACTIVE_MODE = PEN_MODE);
+
